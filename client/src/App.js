1994ab4a9652279ed624506ce1e6b7657c258979
Imaparadox <stevemarsh117@gmail.com>
2021-09-13 11:40:32 -0400

    Updated client App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,30 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-const newClient = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem('id_token');
-
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      }
-    });
-  },
-  uri: '/graphql'
-})
-
 function App() {
+
+  const httpLink = createHLink({
+    uri: '/graphql',
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+  const newClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
   return (
     <ApolloProvider client={newClient}>
       <Router>
