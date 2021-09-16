aff957155bd590a520af00d55ffab2b97db49824
Imaparadox <stevemarsh117@gmail.com>
2021-09-15 20:36:03 -0400

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 //   useFindAndModify: false,
 // });
 // const URI = process.env.MONGODB_URL;
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/googlebooks',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
   err => {
     if (err) throw err;
     console.log('connected to MongoDB')
